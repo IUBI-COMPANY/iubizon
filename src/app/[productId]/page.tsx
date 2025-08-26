4cd030970e624416aaf38e8a5070272dc096b62b
Noel Moriano <nmoriano26@gmail.com>
2025-08-25 21:35:02 -0500

    Updated padding of media item for desktop

--- src/app/[productId]/page.tsx
+++ src/app/[productId]/page.tsx
@@ -49,7 +49,7 @@ export default function ProductDetailPage() {
         </div>
       </header>
       <div className="content-wrapper px-7 max-w-6xl m-auto w-full">
-        <main className="grid grid-cols-12 md:px-12 py-15 w-full">
+        <main className="grid grid-cols-12 py-15 w-full">
           <section className="col-span-12 md:col-span-7 w-full flex justify-center items-center">
             <MediaCarousel product={product} />
           </section>
