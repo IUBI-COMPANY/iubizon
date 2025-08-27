0b4544f274e41d06cefcfff3ff40e73fab5b65a3
Pablo Aquise <aquise.pablo@iestpgildaballivian.edu.pe>
2025-08-27 11:23:07 -0500

    add gtag manager script for Google Tag Manager services

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,9 +82,28 @@ export default function RootLayout({
         gtag('config', 'AW-17511349348');
         `}
       </Script>
+      <Script
+        id="google-tag-manager"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8X65GN4');`,
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          id="google-tag-manager-noscript"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8X65GN4"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {children}
         <SpeedInsights />
         <footer className="pt-10 pb-6 px-4 bg-gradient-to-b from-secondary/100 to-secondary/95">
