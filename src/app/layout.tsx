4b785638d61557147906828d9f974d4086104b18
Pablo Aquise <aquise.pablo@iestpgildaballivian.edu.pe>
2025-08-27 00:31:39 -0500

    add gtag ads script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-17511349348"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-17511349348');
+        `}
+    </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
