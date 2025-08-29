561e1f6b8869492a21b3fcbcf6938ed82f316ae3
Noel Moriano <45921957+elleon-dev@users.noreply.github.com>
2025-08-29 18:09:36 -0500

    Migrate to multirepo (#26) (#27)
    * Implement multirepo
    
    * Update classifications names
    
    * Update web panel home page
    
    * Refactoring  code
    
    * ...

--- apps/web-client/src/data-list/classifications.ts
+++ apps/web-client/src/data-list/classifications.ts
@@ -7,22 +7,22 @@ interface DetailClassification {
 
 export const classifications: Record<Classification, DetailClassification> = {
   premium: {
-    name: "Modelos Premium",
+    name: "Proyectores Premium",
     description:
       "Proyectores en excelente estado físico y 100% funcionales. Alto brillo, colores nítidos y estética casi impecable. Ideales para clientes exigentes o uso profesional.",
   },
   standard: {
-    name: "Modelos Estándar",
+    name: "Proyectores Estándar",
     description:
       "Proyectores en buen estado funcional y físico aceptable. Pueden tener ligeros detalles estéticos, pero ofrecen un rendimiento confiable para uso frecuente.",
   },
   budget: {
-    name: "Modelos Económicos",
+    name: "Proyectores Económicos",
     description:
       "Proyectores económicos, con funcionamiento correcto pero con más detalle físico o limitaciones de brillo. Opción accesible para quienes buscan bajo costo.",
   },
   clearance: {
-    name: "Modelos en Remate",
+    name: "Proyectores en Remate",
     description:
       "Proyectores en liquidación. Funcionales pero con detalles de manchas o detalles menores estéticos, vendidos a precios reducidos sin garantía extendida.",
   },
