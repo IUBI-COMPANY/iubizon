c79ef81bfad1a0bb7d63d09fdb31241a8d9726c4
Noel Moriano <nmoriano26@gmail.com>
2026-08-03 20:52:54 -0500

    refactor: disable unused variable and img element rules in ESLint configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -211,7 +211,6 @@ export default function RootLayout({
           />
         </noscript>
         <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             height="1"
             width="1"
