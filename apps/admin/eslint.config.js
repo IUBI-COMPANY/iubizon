50757ff9e9dd204f42722638ebd0154b3fca0d12
Noel Moriano <nmoriano26@gmail.com>
2026-08-08 22:06:05 -0500

    Format code

--- apps/admin/eslint.config.js
+++ apps/admin/eslint.config.js
@@ -9,7 +9,13 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  { rules: { "@typescript-eslint/no-explicit-any": "off", "@typescript-eslint/no-unused-vars": "warn", "@next/next/no-img-element": "off" } },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
