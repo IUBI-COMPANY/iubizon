d91e86a01b7133bd28b3aae6749206b938a292f8
Noel Moriano <nmoriano26@gmail.com>
2026-08-13 22:44:24 -0500

    Format code

--- apps/web/src/components/features/layout/Footer.tsx
+++ apps/web/src/components/features/layout/Footer.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Facebook, Instagram, Phone, Mail, MapPin, Landmark } from "lucide-react";
+import {
+  Facebook,
+  Instagram,
+  Phone,
+  Mail,
+  MapPin,
+  Landmark,
+} from "lucide-react";
 import type { Category } from "@/types";
 
 interface FooterProps {
